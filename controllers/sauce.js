a3bad402a290f2bbc89b79f0d78935e0676c7f8c
BlockProgram <blockprogram@protonmail.com>
2020-05-10 16:39:38 +0200

    publish like feature

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -20,41 +20,55 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.likeSauce = (req, res, next) => {
-  // SEE BELOW
-  // https://docs.mongodb.com/manual/reference/operator/update/pull/#up._S_pull
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      let likeUsers = Array.from(sauce.usersLiked);
-      let dislikeUsers = Array.from(sauce.usersDisliked);
-      if ((req.body.like = 1)) {
-        likeUsers.push(req.body.userId);
-        Sauce.updateOne(
-          { _id: req.params.id },
-          {
-            $inc: { likes: 1 },
-            usersLiked: likeUsers,
-          }
-        )
-          .then(() => res.status(200).json({ message: "Like appliqué " }))
-          .catch((error) => res.status(400).json({ error }));
-      } else if ((req.body.like = -1)) {
-        dislikeUsers.push(req.body.userId);
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { $inc: { dislikes: 1 }, usersDisliked: dislikeUsers }
-        )
-          .then(() => res.status(200).json({ message: "Dislike appliqué" }))
-          .catch((error) => res.status(400).json({ error }));
+  // User likes
+  if (req.body.like === 1) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        $addToSet: { usersLiked: req.body.userId },
       }
-    })
-    .catch((error) => res.status(400).json({ error }));
+    )
+      .then(() => res.status(200).json({ message: "User liked ajouté " }))
+      .catch((error) => res.status(400).json({ error }));
 
-  //   Définit le statut "j'aime" pour userID fourni. Si j'aime = 1, l'utilisateur aime la sauce.
-  // Si j'aime = 0, l'utilisateur annule ce qu'il aime ou ce qu'il n'aime pas. Si j'aime = -1,
-  // l'utilisateur n'aime pas la sauce. L'identifiant de l'utilisateur doit être ajouté
-  // ou supprimé du tableau approprié, en gardant une trace de ses préférences et en
-  // l'empêchant d'aimer ou de ne pas aimer la même sauce plusieurs fois. Nombre total
-  //  de "j'aime" et de "je n'aime pas" à mettre à jour avec chaque "j'aime".
+    // User dislikes
+  } else if (req.body.like === -1) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        $addToSet: { usersDisliked: req.body.userId },
+      }
+    )
+      .then(() => res.status(200).json({ message: "User disliked ajouté" }))
+      .catch((error) => res.status(400).json({ error }));
+
+    // User removes like/dislike
+  } else if (req.body.like === 0) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        $pull: { usersLiked: req.body.userId, usersDisliked: req.body.userId },
+      }
+    )
+      .then(() => res.status(200).json({ message: "Vote modifié" }))
+      .catch((error) => res.status(400).json({ error }));
+  }
+
+  //Update likes/dislikes numbers
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    console.log(`likes: ${sauce.usersLiked.length}`);
+    console.log(`dislikes: ${sauce.usersDisliked.length}`);
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        likes: `${sauce.usersLiked.length}`,
+        dislikes: `${sauce.usersDisliked.length}`,
+      }
+    )
+
+      .then(() => console.log("success"))
+      .catch((error) => console.error(error));
+  });
 };
 
 exports.getAllSauces = (req, res, next) => {
